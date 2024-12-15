37e94c05ca8554c4199e927e136c24e174ca260a
Davide Dal Pos <daveliga@gmail.com>
2024-12-15 10:00:35 -0500

    Update

--- pages/news.vue
+++ pages/news.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto py-4">
-    <h1 class="text-4xl font-bold">Recent</h1>
+    <h1 class="text-4xl font-bold">Recent taxa added</h1>
     <div class="flex flex-col gap-4 mt-4">
       <RecentTaxonTable
         :parameters="{

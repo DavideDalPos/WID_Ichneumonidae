f2466343f7eb3670f722f1473048fe2d5b0549b1
Davide Dal Pos <daveliga@gmail.com>
2024-12-15 09:53:32 -0500

    Update

--- pages/components/Section/SectionCollaboration.vue
+++ pages/components/Section/SectionCollaboration.vue
@@ -4,8 +4,7 @@
       <h2 class="px-4 sm:px-0">Collaborate with us</h2>
       <div class="flex flex-col gap-6">
         <div class="p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-sm">
-          <h3 class="text-xl font-semibold mt-2 mb-1">Ichneumonid meetings online</h3>
-          <span class="text-sm text-gray-500 dark:text-gray-400">2023-10-27</span>
+          <h3 class="text-xl font-semibold mt-2 mb-1">Looking for collaboration!!</h3>
           <p class="mt-2">
             If you are a researcher or an institution wishing to collaborate with the World Ichneumonidae Database, 
             please send an email to the Project Coordinator, Rachel Behm (behmrachel@yahoo.com).
@@ -13,8 +12,7 @@
         </div>
 
         <div class="p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-sm">
-          <h3 class="text-xl font-semibold mt-2 mb-1">Ichneumonid meetings online</h3>
-          <span class="text-sm text-gray-500 dark:text-gray-400">2023-10-27</span>
+          <h3 class="text-xl font-semibold mt-2 mb-1">Looking for type specimens</h3>
           <p class="mt-2">
             Call for type specimens! Does your institution have Ichneumonid type specimens? We want to work with you 
             to incorporate that info into our database. Please email WID Administrator, Davide Dal Pos (daveliga@gmail.com).

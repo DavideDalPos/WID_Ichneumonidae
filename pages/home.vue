7dea13cff135f8153819be085df8bf6a6f45672e
Davide Dal Pos <daveliga@gmail.com>
2025-07-29 18:36:53 -0400

    updates

--- pages/home.vue
+++ pages/home.vue
@@ -3,17 +3,15 @@
       <SectionLandscape />
       <SectionData />
       <hr class="border-base-muted" />
-      <SectionDescription />
-      <SectionAuthor class="shadow-sm" />
-      <hr class="border-base-muted" />
+      <SectionDescription class="border-b"/>
       <SectionAnnouncements />
       <SectionTerms class="drop-shadow" />
     </div>
   </template>
   <script setup>
-  import SectionData from './components/Section/SectionData.vue'
-  import SectionLandscape from './components/Section/SectionLandscape.vue'
-  import SectionDescription from './components/Section/SectionDescription.vue'
-  import SectionTerms from './components/Section/SectionTerms.vue'
   import SectionAnnouncements from './components/Section/SectionAnnouncements.vue'
+import SectionData from './components/Section/SectionData.vue'
+import SectionDescription from './components/Section/SectionDescription.vue'
+import SectionLandscape from './components/Section/SectionLandscape.vue'
+import SectionTerms from './components/Section/SectionTerms.vue'
   </script>

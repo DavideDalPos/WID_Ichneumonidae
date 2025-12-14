341065a585914afea5a245496b82a20f36af4965
Davide Dal Pos <daveliga@gmail.com>
2025-12-14 17:09:23 -0500

    Updatedfinal

--- pages/UnderConstruction.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="h-full">
-      <UnderConstruction_placeholder />
-    </div>
-  </template>
-  <script setup>
-import UnderConstruction_placeholder from './components/Section/Description/UnderConstruction_placeholder.vue';
-  </script>
